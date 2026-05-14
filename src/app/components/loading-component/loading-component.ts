d6eb58b20bd1b6c739edbf8cf1e5b7fddfc9513d
Marco Cordero <cordero_inq@outlook.com>
2026-05-13 23:54:07 -0600

    Loading animation start

---
+++ src/app/components/loading-component/loading-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pizza-alien-loading-component',
+  imports: [],
+  templateUrl: './loading-component.html',
+  styleUrl: './loading-component.scss',
+})
+export class LoadingComponent {}
